c4c37fe57eea94e6976463a23116179807ad153c
FernandoPucci <fsilvapucci@gmail.com>
2016-11-27 17:18:06 -0200

    Configurações do Jersey para novas classes REST

--- vendas/src/main/java/br/com/exemplo/vendas/JerseyConfig.java
+++ vendas/src/main/java/br/com/exemplo/vendas/JerseyConfig.java
@@ -4,20 +4,25 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
 
 import br.com.exemplo.vendas.ws.HealthCheckApiController;
+import br.com.exemplo.vendas.ws.ItemVendaApiController;
+import br.com.exemplo.vendas.ws.ProdutoApiController;
 import br.com.exemplo.vendas.ws.TipoEmbalagemApiController;
+import br.com.exemplo.vendas.ws.VendaApiController;
 
 /**
  * @author fernandopucci
  * 
- * Classe de configuração dos serviços REST
+ *         Classe de configuração dos serviços REST
  */
 @Configuration
 public class JerseyConfig extends ResourceConfig {
-	
-	
-		public JerseyConfig() {
-			register(TipoEmbalagemApiController.class);
-			register(HealthCheckApiController.class);
+
+	public JerseyConfig() {
+		register(TipoEmbalagemApiController.class);
+		register(HealthCheckApiController.class);
+		register(ItemVendaApiController.class);
+		register(ProdutoApiController.class);
+		register(VendaApiController.class);
 
 	}
 }
